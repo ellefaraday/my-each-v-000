acc27e22a23265a687dad7d6759a286771e6b522
Elle Faraday <faraday.elle@gmail.com>
2017-10-28 21:01:55 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
-  # code here
+def my_each (array)
+  i = 0
+if block?
+  while i<array.length
+    yield(array[i])
+    i +=1
+else
+  puts "Missing a block!"
 end
+  array
+end
+
+my_each(array) {|item| puts item}
+
+
+
